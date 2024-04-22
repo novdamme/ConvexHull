be8601f3fbd75cdcb756980346b077958fed5857
Palle <palle.maesen@hotmail.com>
2024-04-22 16:35:38 +0200

    fixed merge issues

--- src/quickHullDisk/SilverTriangleFilterUtil.java
+++ src/quickHullDisk/SilverTriangleFilterUtil.java
@@ -1,12 +1,21 @@
 package quickHullDisk;
 
 import dto.Disk;
+import dto.Line;
+import dto.Pair;
 import dto.Point;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class SilverTriangleFilterUtil {
 
+    public enum SilverConfig {
+            CASE_A,
+            CASE_B,
+            CASE_C1,
+            CASE_C2
+    };
     public static boolean triangleFilterIsSilver(
             int numOfDisks,
             int numOfDisksOnFrontEdge,
@@ -20,6 +29,35 @@ public class SilverTriangleFilterUtil {
         );
     }
 
+    public static SilverConfig getSilverTriangleConfiguration(List<Disk> disks, Line orientedLinePQ, Disk startDisk, Disk endDisk, Disk apex) {
+         double height = apex.getRadius() - orientedLinePQ.getDistance(apex.getCenter()); 
+         if (height == 0) {
+                 List<Disk> nonPositiveDisks = DisksUtil.findExpandedNonPositiveDisks(disks, orientedLinePQ, startDisk, endDisk);
+                 if (nonPositiveDisks.size() > 2) {
+                         return SilverConfig.CASE_A;
+                 } else {
+                         return SilverConfig.CASE_B;
+                 }
+         } else {
+                return SilverConfig.CASE_C1;
+                 // } else {
+                 //         return SilverConfig.CASE_C2;
+                 // }
+         }
+    } 
+
+    /**
+    * regularizes silver triangles 
+    * @param disks
+    * @param preApexDisk
+    * @param postApexDisk
+    * @param hullPointP
+    * @param hullPointQ
+    * @param frontEdgeDisks
+    * @param backEdgeDisks
+    * @param possibleApexDisks
+    * @return A Pair object containing the pivot disk and the pivot point
+    **/
     public static void regularizeSliverTriangleNPivotDisks(
             List<Disk> disks,
             Disk preApexDisk,
@@ -31,6 +69,28 @@ public class SilverTriangleFilterUtil {
             Disk apexDisk,
             Point triangleApexX
             ) {
-        // TODO
+
+        Line orientedNonNegativeTangentLine = new Line(null, null);
+        SilverConfig config = getSilverTriangleConfiguration(disks, orientedNonNegativeTangentLine, preApexDisk, postApexDisk, apexDisk);
+
+        switch(config) {
+                case CASE_A: {
+                        triangleApexX = orientedNonNegativeTangentLine.getStart(); 
+                        apexDisk = preApexDisk;
+                        
+                } case CASE_B : {
+                        List<Disk> nonPositiveDisks = DisksUtil.findExpandedNonPositiveDisks(disks, orientedNonNegativeTangentLine, preApexDisk, postApexDisk);
+                        for (Disk d : disks) {
+                                if (!d.equals(preApexDisk) && !d.equals(postApexDisk) && !nonPositiveDisks.contains(d)) {
+                                        apexDisk = d;      
+                                }
+                        }
+                        break;
+                } case CASE_C1: {
+                        break;
+                } case CASE_C2 : {
+                        break;
+                }
+        }
     }
 }
