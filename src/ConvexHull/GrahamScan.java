f72b2d0984fe0fe07333755637fb32230df7df8e
Kobe <kobebleuze@live.be>
2024-04-23 02:59:27 +0200

    GrahamScan reformat + cleanup
    use dto.point instead of Point2D

--- src/ConvexHull/GrahamScan.java
+++ src/ConvexHull/GrahamScan.java
@@ -1,10 +1,12 @@
 package ConvexHull;
 
-import java.awt.geom.Point2D.Double;
+import dto.Point;
+
 import java.util.*;
 
 public class GrahamScan {
     private static final Random rand = new Random(0);
+    private Point lowest;
 
     public static void main(String[] args) {
         GrahamScan grahamScan = new GrahamScan();
@@ -14,56 +16,39 @@ public class GrahamScan {
         }
         List<Point> sol = grahamScan.getConvexHull(points);
         for (Point p : sol) {
-            System.out.println(p);
+            System.out.println(p.getX() + " " + p.getY());
         }
     }
 
     public List<Point> getConvexHull(List<Point> points) {
-        Point lowest = getLowestPoint(points);
-        points.sort(Comparator.comparingDouble((Point a) -> a.getAngle(lowest)).thenComparingDouble((a) -> a.getDistance(lowest)));
+        lowest = Collections.min(points, Comparator.comparingDouble(Point::getY).thenComparingDouble(Point::getX));
+        points.sort(Comparator.comparingDouble(this::getAngle).thenComparingDouble(this::getDistance));
         Stack<Point> stack = new Stack<>();
         stack.push(points.get(0));
         stack.push(points.get(1));
         for (int i = 2; i < points.size(); i++) {
-            Point head = points.get(i);
-            Point middle = stack.pop();
-            Point tail = stack.peek();
-            double crossProduct = ((middle.x - tail.x) * (head.y - tail.y)) - ((middle.y - tail.y) * (head.x - tail.x));
+            Point p1 = points.get(i);
+            Point p2 = stack.pop();
+            Point p3 = stack.peek();
+            double crossProduct = ((p2.getX() - p3.getX()) * (p1.getY() - p3.getY())) - ((p2.getY() - p3.getY()) * (p1.getX() - p3.getX()));
             if (crossProduct > 0) {
-                stack.push(middle);
-                stack.push(head);
+                stack.push(p2);
+                stack.push(p1);
             } else if (crossProduct < 0) {
                 i--;
             } else {
-                stack.push(head);
+                stack.push(p1);
             }
         }
         Collections.reverse(stack);
         return new ArrayList<>(stack);
     }
 
-    private Point getLowestPoint(List<Point> points) {
-        Point lowest = points.get(0);
-        for (int i = 1; i < points.size(); i++) {
-            Point temp = points.get(i);
-            if (temp.y < lowest.y || (temp.y == lowest.y && temp.x < lowest.x)) {
-                lowest = temp;
-            }
-        }
-        return lowest;
+    public double getAngle(Point p1) {
+        return Math.atan2(p1.getY() - lowest.getY(), p1.getX() - lowest.getX());
     }
 
-    private static class Point extends Double {
-        public Point(double x, double y) {
-            super(x, y);
-        }
-
-        public double getAngle(Point p) {
-            return Math.atan2(y - p.y, x - p.x);
-        }
-
-        public double getDistance(Point p) {
-            return Math.sqrt(((p.x - x) * (p.x - x)) + ((p.y - y) * (p.y - y)));
-        }
+    public double getDistance(Point p1) {
+        return Math.sqrt(Math.pow(lowest.getX() - p1.getX(), 2) + Math.pow(lowest.getY() - p1.getY(), 2));
     }
 }
